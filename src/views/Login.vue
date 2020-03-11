6502461a1463fdccaa828c67b7be9c26a2658407
RafaelRTL <j_rafaelpilotzi@hotmail.com>
2020-03-11 14:11:17 -0600

    agregando login.vue. Borrado de about

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login">
+    <input type="text" placeholder="Email"><br>
+    <input type="password" placeholder="Password"><br>
+    <button>Conexión</button>
+    <p>¿No tienes cuenta? Crea una</p>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    name: 'login',
+    data () {
+      return {};
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
