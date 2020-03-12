6c3aec2b126afc8e58c6caa1a3715b6d6abc5d8b
RafaelRTL <j_rafaelpilotzi@hotmail.com>
2020-03-11 20:01:31 -0600

    agregando router-link a login y sign-up

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@
     <input type="text" placeholder="Email"><br>
     <input type="password" placeholder="Password"><br>
     <button>Conexión</button>
-    <p>¿No tienes cuenta? Crea una</p>
+    <p>¿No tienes cuenta? <router-link to="/sign-up"> Crea una </router-link> </p>
   </div>
 </template>
 
@@ -17,6 +17,26 @@
   }
 </script>
 
-<style scoped>
-
+<style scoped> /* "scoped" attribute limit CSS to this component only*/
+  .login{
+    margin-top: 40px;
+  }
+  input{
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+  button{
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+  p{
+    margin-top: 40px;
+    font-size: 13px;
+  }
+  p a{
+    text-decoriation: underline;
+    cursor: pointer;
+  }
 </style>
