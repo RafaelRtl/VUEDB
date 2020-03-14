eff7b5683f9de3493f6f9377de7a928b833afe4f
RafaelRTL <j_rafaelpilotzi@hotmail.com>
2020-03-13 19:48:31 -0600

    agregando firebase. Existe un error por corregir

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import SignUp from '../views/SignUp.vue'
 
+
 Vue.use(VueRouter)
 
 const routes = [
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,20 +1,36 @@
 <template>
 <div class="sign-up">
   <p>¡vamos a crear una cuenta nueva!</p>
-  <input type="text" placeholder="Email"><br>
-  <input type="password" placeholder="Password"><br>
-  <button>Sign Up</button>
+  <input type="text" v-model="email" placeholder="Email"><br>
+  <input type="password" v-model="password" placeholder="Password"><br>
+  <button @click="signUp">Sign Up</button>
   <span>o regresa al <router-link to="/login"> inicio de sesión </router-link></span>
 </div>
 </template>
 
 <script>
+
+  import firebase from 'firebase';
+
   export default{
     name: 'signUp',
     data() {
-      return {}
+      return {
+        email: '',
+        password:''
+      }
     },
-    methods: {}
+    methods: {
+      signUp: function () {
+        firebase.auth().createUserWithEmailAndPassword(this.email,this.password).then(function (user){
+            alert('¡Se ha creado la cuenta!')
+          },
+          function (err){
+            alert('No se creo la cuenta ' + err.message)
+          }
+        );
+      }
+    }
   }
 </script>
 
